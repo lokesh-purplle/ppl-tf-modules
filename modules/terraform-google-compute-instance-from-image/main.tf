d86ed2ef5cfa85cfea44fc5c68c97abc2c3cfcae
lokesh.j1 <lokesh.j@purplle.com>
2024-02-07 18:11:14 +0530

    updated the module to to use external gcp service account

--- modules/terraform-google-compute-instance-from-image/main.tf
+++ modules/terraform-google-compute-instance-from-image/main.tf
@@ -57,12 +57,6 @@ resource "google_compute_attached_disk" "attach_storage_disk" {
   instance    = google_compute_instance_from_machine_image.instance.self_link
 }
 
-# Create SA
-resource "google_service_account" "service_account" {
-  count        = var.service_account == "" ? 1 : 0
-  account_id   = "${var.instance_name}-vm-sa"
-  display_name = "${var.instance_name}-vm-sa"
-}
 
 # Create an external IP for the instance
 resource "google_compute_address" "external_ip" {
@@ -97,7 +91,7 @@ enable_secure_boot = true
 }
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = var.service_account == "" ? google_service_account.service_account[0].email : var.service_account
+    email  = var.service_account
     scopes = ["cloud-platform"]
   }
 
@@ -126,7 +120,7 @@ enable_secure_boot = true
     startup-script = var.disk_storage_enabled ? file("${path.module}/init/mnt_dir.sh") : null
     MOUNT_DIR      = var.disk_storage_mount_path
     REMOTE_FS      = "/dev/sdb"
-    ssh-keys = "ubuntu:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC3S8sI+j1F2hwtItaE4o3qn4B08aTNN3+hA47UlorDEsnmmbJmoduuGgVtrIDUxX7FVYi5ACq35fs3vzOFaWI6CcrePckuB+zJcyyddjMpNvZZjCpyX++LRNZjq3ltzBdNt0Aa8+8SLuWmlPrZxDwa8CBdo1e1T3Zjbplps1Zt1v7ri28UPBMyL6mnCGglGr+bKgQrPm41phcPxw7GmdPDQhxrxdNRdlQG+iM8AbYUfQbRj60mf88lQFmS1hFz5qPx/6e7FcJwkDeWUJkXzRz5/aSpxxHVt4wPyZ+8LScOd4WJxkhUo38xBhrZLsZWxj2UhrCvbRoIeCpDE+DqtFEiR7wDVFO6XHbEwdNGKZ7Zc1Zv6+U7bNJglovI7qZsZESTNrQ/8rs6VxJRQwXBiLRnzJdRcMmpjIvr5P3Jl34N/Ixv3vulsP8RhhVJg2lydoyc1d97J36iI3kHccuuUaGI+GsPRZ/QbDxC9e7ysiJD53R5CCQsdYJPT5TZnS2JXls= ubuntu@purplle.com"
+    ssh-keys = "ubuntu:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCrWtOAD39kvgQCSrFbNARmvJVwVUOrMZH9RVkJXcm4Bo72xVTDw/bLz1psTqUBT3uQFIlkDfxyI4vf5CdKUofIeexsax6ant7Eh7xNzSkPycEJIoktCwAU86IbIC+awGUTCMWI4pdku9lzVWHsMiuSr9jLLCnh9Z9fO/Z+nOrPvZIRfWzPYO57LAAA9R28qDIvw7KZWSuVLc1mdfRVzdSlgQUUHNRGxhrvVJ0+jA56PvbSvdulck3wUBP81MvKuarEEvkUxDteGxPyz+Op1YKIDZCF30fzOjg6V5X8xlTJ5GzwSikRabH8KDt4p8FYbDGf7JS7Y1xA69/6AltzzHUXn6bCIAAT+JRyFh1m715THGLLnw8ez0X8V42Z62bL1LvLf8QLp3liwMXjMUZzexqOH2YUA/iDV9wMu52tY0FrWkdrxjhd4Io534474x5tQt+WfdyqGv3QHbP+HWGPxUG28Iwm6H1qM/BSBwBpbgvvkxwOSFwcAvOmfJQueUwboWM= ubuntu@jenkins-automation-sandbox"
   }
 
   # Tags in GCP are only used for network and firewall rules. Any metadata is
