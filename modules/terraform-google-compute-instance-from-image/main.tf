5511ee5a4f843fafc632022dab291b934f6f3c92
lokesh-purplle <129851301+lokesh-purplle@users.noreply.github.com>
2024-04-17 14:35:58 +0530

    Update main.tf for block project wide ssh key

--- modules/terraform-google-compute-instance-from-image/main.tf
+++ modules/terraform-google-compute-instance-from-image/main.tf
@@ -117,10 +117,14 @@ enable_secure_boot = true
 
   # Execute the script to format & mount /var/opt
   metadata = {
+    block-project-ssh-keys = true
     startup-script = var.disk_storage_enabled ? file("${path.module}/init/mnt_dir.sh") : null
     MOUNT_DIR      = var.disk_storage_mount_path
     REMOTE_FS      = "/dev/sdb"
-    ssh-keys = "ubuntu:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCrWtOAD39kvgQCSrFbNARmvJVwVUOrMZH9RVkJXcm4Bo72xVTDw/bLz1psTqUBT3uQFIlkDfxyI4vf5CdKUofIeexsax6ant7Eh7xNzSkPycEJIoktCwAU86IbIC+awGUTCMWI4pdku9lzVWHsMiuSr9jLLCnh9Z9fO/Z+nOrPvZIRfWzPYO57LAAA9R28qDIvw7KZWSuVLc1mdfRVzdSlgQUUHNRGxhrvVJ0+jA56PvbSvdulck3wUBP81MvKuarEEvkUxDteGxPyz+Op1YKIDZCF30fzOjg6V5X8xlTJ5GzwSikRabH8KDt4p8FYbDGf7JS7Y1xA69/6AltzzHUXn6bCIAAT+JRyFh1m715THGLLnw8ez0X8V42Z62bL1LvLf8QLp3liwMXjMUZzexqOH2YUA/iDV9wMu52tY0FrWkdrxjhd4Io534474x5tQt+WfdyqGv3QHbP+HWGPxUG28Iwm6H1qM/BSBwBpbgvvkxwOSFwcAvOmfJQueUwboWM= ubuntu@jenkins-automation-sandbox"
+    ssh-keys = <<EOT
+                ubuntu:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCrWtOAD39kvgQCSrFbNARmvJVwVUOrMZH9RVkJXcm4Bo72xVTDw/bLz1psTqUBT3uQFIlkDfxyI4vf5CdKUofIeexsax6ant7Eh7xNzSkPycEJIoktCwAU86IbIC+awGUTCMWI4pdku9lzVWHsMiuSr9jLLCnh9Z9fO/Z+nOrPvZIRfWzPYO57LAAA9R28qDIvw7KZWSuVLc1mdfRVzdSlgQUUHNRGxhrvVJ0+jA56PvbSvdulck3wUBP81MvKuarEEvkUxDteGxPyz+Op1YKIDZCF30fzOjg6V5X8xlTJ5GzwSikRabH8KDt4p8FYbDGf7JS7Y1xA69/6AltzzHUXn6bCIAAT+JRyFh1m715THGLLnw8ez0X8V42Z62bL1LvLf8QLp3liwMXjMUZzexqOH2YUA/iDV9wMu52tY0FrWkdrxjhd4Io534474x5tQt+WfdyqGv3QHbP+HWGPxUG28Iwm6H1qM/BSBwBpbgvvkxwOSFwcAvOmfJQueUwboWM= ubuntu@jenkins-automation-sandbox
+                lokesh.j:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC3S8sI+j1F2hwtItaE4o3qn4B08aTNN3+hA47UlorDEsnmmbJmoduuGgVtrIDUxX7FVYi5ACq35fs3vzOFaWI6CcrePckuB+zJcyyddjMpNvZZjCpyX++LRNZjq3ltzBdNt0Aa8+8SLuWmlPrZxDwa8CBdo1e1T3Zjbplps1Zt1v7ri28UPBMyL6mnCGglGr+bKgQrPm41phcPxw7GmdPDQhxrxdNRdlQG+iM8AbYUfQbRj60mf88lQFmS1hFz5qPx/6e7FcJwkDeWUJkXzRz5/aSpxxHVt4wPyZ+8LScOd4WJxkhUo38xBhrZLsZWxj2UhrCvbRoIeCpDE+DqtFEiR7wDVFO6XHbEwdNGKZ7Zc1Zv6+U7bNJglovI7qZsZESTNrQ/8rs6VxJRQwXBiLRnzJdRcMmpjIvr5P3Jl34N/Ixv3vulsP8RhhVJg2lydoyc1d97J36iI3kHccuuUaGI+GsPRZ/QbDxC9e7ysiJD53R5CCQsdYJPT5TZnS2JXls= lokesh.j@purplle.com
+    EOT
   }
 
   # Tags in GCP are only used for network and firewall rules. Any metadata is
